19544cccebf24854fbbca5e7fedea457481a6025
pigo <pigo@jumbo.homenet.telecomitalia.it>
2014-05-22 01:26:01 +0200

    Simone::BTF::Day3

---
+++ analysis_tools.h
@@ -0,0 +1,107 @@
+/*************************************************************
+
+*************************************************************/
+
+#ifndef __analysis_tools__
+#define __analysis_tools__
+
+#include <cstdio>
+#include <cstdlib>
+#include <stdint.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+#include "TTree.h"
+
+using namespace std;
+
+#define DIGITIZER_SAMPLING_UNIT 0.2 //digitizer samples width (ns)
+
+//---estimate the baseline in a given range and then subtract it from the signal 
+float SubtractBaseline(int tb1, int tb2, vector<float>* samples)
+{
+    float baseline=0;
+    int minSample=tb1;
+    //---compute baseline
+    for(int iSample=tb1; iSample<tb2; iSample++)
+    {
+        baseline += samples->at(iSample);
+        if(samples->at(iSample) < samples->at(minSample)) 
+            minSample = iSample;
+    }
+    baseline = baseline/(tb2-tb1);
+    //---subtract baseline
+    for(int iSample=0; iSample<samples->size(); iSample++)
+    {
+        samples->at(iSample) = samples->at(iSample) - baseline;
+    }
+    return samples->at(minSample);
+}
+
+//---estimate time (ns) with CFD, samples must be a negative signal and baseline subtract
+float TimeConstFrac(int t1, int t2, const vector<float>* samples, float AmpFraction, int Nsamples = 5)
+{
+
+    float xx= 0.;
+    float xy= 0.;
+    float Sx = 0.;
+    float Sy = 0.;
+    float Sxx = 0.;
+    float Sxy = 0.;
+    float Chi2 = 0.;
+    int minSample=0;
+    int cfSample=0; // first sample over AmpMax*CF 
+    float minValue=0;
+    
+    for(int iSample=t1; iSample<t2; iSample++)
+    {
+        if(samples->at(iSample) < samples->at(minSample)) minSample = iSample;
+    }
+    minValue = samples->at(minSample);
+    for(int iSample=t1; iSample<t2; iSample++)
+    {
+        if(samples->at(iSample) > minValue*AmpFraction) cfSample = iSample;
+    }
+    for(int n=-(Nsamples-1)/2; n<=(Nsamples-1)/2; n++)
+    {
+        if(cfSample+n<0) continue;
+        xx = (cfSample+n)*(cfSample+n)*0.2*0.2;
+        xy = (cfSample+n)*0.2*(samples->at(cfSample+n));
+        Sx = Sx + (cfSample+n)*0.2;
+        Sy = Sy + samples->at(cfSample+n);
+        Sxx = Sxx + xx;
+        Sxy = Sxy + xy;
+    }
+
+    float Delta = Nsamples*Sxx - Sx*Sx;
+    float A = (Sxx*Sy - Sx*Sxy) / Delta;
+    float B = (Nsamples*Sxy - Sx*Sy) / Delta;
+
+    float sigma2 = pow(0.2/sqrt(12)*B,2);
+ 
+    for(int n=-(Nsamples-1)/2; n<=(Nsamples-1)/2; n++)
+    {
+        if(cfSample+n<0) continue;
+        Chi2 = Chi2 + pow(samples->at(cfSample+n) - A - B*((cfSample+n)*0.2),2)/sigma2;
+    } 
+    // A+Bx = AmpFraction * amp
+    float interpolation = (samples->at(minSample) * AmpFraction - A) / B;
+    return interpolation;
+}
+
+float AmpMax(int t1, int t2, const vector<float>* samples)
+{
+    int minSample=t1;
+    for(int iSample=t1; iSample<t2; iSample++)
+    {
+        if(samples->at(iSample) < samples->at(minSample)) minSample = iSample;
+    }
+    return samples->at(minSample);
+}
+    
+#endif
